195d9838c0141014df981d0fa7328f0614c6595a
Rita Grou <131721145+Ritagrou@users.noreply.github.com>
2023-04-24 23:43:55 +0100

    Add files via upload

---
+++ index/script.js
@@ -0,0 +1,12 @@
+function openLink1() {
+    window.open('https://www.instagram.com/ritagr0u/', '_blank');
+  }
+  
+  function openLink2() {
+    window.open('https://www.linkedin.com/in/rita-grou/', '_blank');
+  }
+  
+  function openLink3() {
+    window.open('https://twitter.com/ritagrou', '_blank');
+  }
+  
